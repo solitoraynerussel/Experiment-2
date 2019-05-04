061b75d4935d2008d53cd86987cfe279724d393b
solitoraynerussel <raynerussel.solito.eng@ust.edu.ph>
2019-05-04 16:17:23 +0800

    1st

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -11,39 +11,59 @@ int main()
  int time;
  int price;
  
- 	cout << "Choose between these packages A, B, and C\n";
+ 	cout << "Choose between these packages\n";
 	cout << "Package A: For P995/mo 10 hrs of access are provided. Additional hours are P20/hr.\n";
 	cout << "Package B: For P1495/mo 20 hrs of access are provided. Additional hours are P10/hr\n";
 	cout << "Package C: For P1995/mo of unlimited access is provided." <<endl;
+	cout <<"\n";
  
 	 cout << "Enter your chosen package: ";
 	 cin >> package;
+	 
 
- if ((package == "A") || (package == "B") || (package == "C")) {
- cout << "Number of hours used: ";
- cin >> time;
+ if ((package == "A") || (package == "B") || (package == "C")) 
+{
 
-if (package == "A") {
-if (time > 10) {
+if (package == "A") 
+{
+cout << "Number of hours used: ";
+cin >> time;
+if (time > 10)
+{	
 price = 995 + (time - 10) * 20;
-} else {
+cout << "Total charges is equal to: P" << price;
+}
+else 
+{
 price = 995;
+cout << "Total charges is equal to: P" << price;
+}
 }
 
-} else if (package == "B") {
-if (time > 20) {
+else if (package == "B") 
+{
+cout << "Number of hours used: ";
+cin >> time;
+if (time > 20) 
+{
 price = 1495 + (time - 20) * 10;
-} else {
+cout << "Total charges is equal to: P" << price;
+} else
+{
 price = 1495;
+cout << "Total charges is equal to: P" << price;
 }
 
-} else {
+} else if (package == "C")
+{
 price = 1995;
+cout << "Total charges is equal to: P" << price;
 }
- } else {
+} 
+else 
+{
  cout << "Invalid package! Choose only A, B and C";
 }
-cout << "Total charges is equal to: P" << price;
 
 getch();
 return 0;
