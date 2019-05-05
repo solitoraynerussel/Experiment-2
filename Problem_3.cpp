da949909a7c49f2193caa51fa30afb5c91b387d3
solitoraynerussel <raynerussel.solito.eng@ust.edu.ph>
2019-05-05 19:55:31 +0800

    3rd

--- Problem_3.cpp
+++ Problem_3.cpp
@@ -1,31 +1,58 @@
 #include <iostream>
 #include <conio.h>
-#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include <math.h>
 
 using namespace std;
 
 int main() 
 {
-	double x, y;
-	double z = 2.5;
+	int a,x;
+	double y,v,b;
+	const double z = 2.5;
 	
 	cout << "input value of x: "; cin >> x;
 	cout << "input value of y: "; cin >> y;
 	
-	if (x == 1 && y > 1 && y < 5)
-		cout << "V = " << x * y * z << endl;
-		
-	else if (x == 1 && y >= 5)
-		cout << "V = " << x + (y / z) << endl;
-		
-	else if (x == 2 && y <= 5)
-		cout << "V = " << abs((x - y) / z) << endl;
-		
-	else if (x == 2 && y > 5)
-		cout << "V = " << x - sqrt(y + z) << endl;
-		
-	else
-		cout << "V = " << x + y + z << endl;
+	switch(x)
+	
+	{
+	case 1:
+		cout << setprecision(2) << fixed << showpoint;
+	if(y > 1 && y < 5)
+	{
+		v = x * y * z;
+		cout << "V = " << v << setw(10) << setprecision(2);
+	}
+	else if(y >= 5)
+	{
+		v = x + (y / z);
+		cout << "V = " << v << setw(10) << setprecision(2);
+	}
+	break;
+	
+	case 2:
+		cout << setprecision(2) << fixed << showpoint;
+	if(y <= 5)
+	{
+		a = (x - y) / z;
+		v = abs (a);
+		cout << "V = " << v << setw(10) << setprecision(2);
+	}
+	else if(y > 5)
+	{
+		b = sqrt (y + z);
+		v = x - b;	
+		cout << "V = " << v << setw(10) << setprecision(2);
+	}
+	break;
+	
+	default:
+		v = x + y + z;
+		cout << "V = " << v << setw(10) << setprecision(2);
+		break;
+	}
 
 	system("pause");
 	getch();
